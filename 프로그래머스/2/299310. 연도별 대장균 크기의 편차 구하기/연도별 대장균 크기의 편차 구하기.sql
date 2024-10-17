f90781b227e0c06bba9b955ea406d668b8126275
이진형 <86180422+Jinnyzinny@users.noreply.github.com>
2024-10-17 17:27:50 +0900

    [level 2] Title: 연도별 대장균 크기의 편차 구하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/2/299310. 연도별 대장균 크기의 편차 구하기/연도별 대장균 크기의 편차 구하기.sql
+++ 프로그래머스/2/299310. 연도별 대장균 크기의 편차 구하기/연도별 대장균 크기의 편차 구하기.sql
@@ -1,11 +1,4 @@
 -- 코드를 작성해주세요
-# select
-# year(differentiation_date) YEAR,
-# # 
-
-# ID
-# from ecoli_data
-# order by year(differentiation_date)
 select YEAR,max_year.max-size_of_colony as YEAR_DEV,ID
 from ecoli_data
 inner join 
