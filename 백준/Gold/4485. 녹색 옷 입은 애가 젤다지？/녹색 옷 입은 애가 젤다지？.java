60767ef8c7be5e9ba02f98847e8d945b71f9a6d5
이진형 <86180422+Jinnyzinny@users.noreply.github.com>
2024-11-27 15:35:31 +0900

    [Gold IV] Title: 녹색 옷 입은 애가 젤다지?, Time: 248 ms, Memory: 23520 KB -BaekjoonHub

--- 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.java
+++ 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.java
@@ -1,63 +1,77 @@
 import java.util.*;
 import java.util.stream.Stream;
+
+import java.awt.Point;
 import java.io.*;
 
-class pos {
-	int r;
-	int c;
-	int cost;
-
-	protected pos(int r, int c, int cost) {
-		super();
-		this.r = r;
-		this.c = c;
-		this.cost = cost;
-	}
-}
-
 public class Main {
-	static int[] dr = { 1, 0, -1, 0 };
-	static int[] dc = { 0, 1, 0, -1 };
-	static int[][] dist;
+	static int N;
+	static int[] dr = { 0, 1, 0, -1 };
+	static int[] dc = { 1, 0, -1, 0 };
+
+	static class Node implements Comparable<Node> {
+		Point point;
+		int score;
+
+		public Node(Point point, int score) {
+			super();
+			this.point = point;
+			this.score = score;
+		}
+
+		@Override
+		public int compareTo(Node o) {
+			// TODO Auto-generated method stub
+			return this.score - o.score;
+		}
+	}
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		// TODO Auto-generated method stub
+
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-		int N = -1;
-		int p_cnt = 1;
+		int count = 1;
 		while ((N = Integer.parseInt(br.readLine())) != 0) {
-
-			int[][] cave = new int[N][N];
+			int[][] table = new int[N][N];
 			for (int i = 0; i < N; i++) {
-				cave[i] = Stream.of(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+				table[i] = Stream.of(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
 			}
+			System.out.println("Problem " + count++ + ": " + Dijkstra(table));
+		}
+	}
 
-			dist = new int[N][N];
-			for (int i = 0; i < N; i++) {
-				Arrays.fill(dist[i], Integer.MAX_VALUE);
+	private static int Dijkstra(int[][] table) {
+		// TODO Auto-generated method stub
+		int[][] coin = new int[N][N];
+		for (int i = 0; i < N; i++) {
+			Arrays.fill(coin[i], Integer.MAX_VALUE);
+		}
+		boolean[][] vis = new boolean[N][N];
+
+		PriorityQueue<Node> pq = new PriorityQueue<>();
+		pq.offer(new Node(new Point(0, 0), table[0][0]));
+
+		while (!pq.isEmpty()) {
+			Node cur = pq.poll();
+
+			Point curPoint = cur.point;
+			int curScore = cur.score;
+
+			if (vis[curPoint.x][curPoint.y]) {
+				continue;
 			}
-			Queue<pos> q = new LinkedList<>();
-			q.add(new pos(0, 0, cave[0][0]));
-			dist[0][0] = cave[0][0];
+			coin[curPoint.x][curPoint.y] = curScore;
+			vis[curPoint.x][curPoint.y] = true;
 
-			while (!q.isEmpty()) {
-				pos cur = q.poll();
-
-				for (int dir = 0; dir < 4; dir++) {
-					int nr = cur.r + dr[dir];
-					int nc = cur.c + dc[dir];
-
-					if (nr >= 0 && nc >= 0 && nr < N && nc < N) {
-						if (cave[nr][nc] + cur.cost < dist[nr][nc]) {
-							dist[nr][nc] = cave[nr][nc] + cur.cost;
-							q.offer((new pos(nr, nc, cur.cost + cave[nr][nc])));
-						}
-					}
+			for (int dir = 0; dir < 4; dir++) {
+				int nr = curPoint.x + dr[dir];
+				int nc = curPoint.y + dc[dir];
+				if (nr >= 0 && nc >= 0 && nr < N && nc < N && coin[nr][nc] > curScore + table[nr][nc]) {
+					pq.offer(new Node(new Point(nr, nc), curScore + table[nr][nc]));
 				}
 			}
-			System.out.println("Problem " + p_cnt + ": " + dist[N - 1][N - 1]);
-			p_cnt++;
 		}
+		return coin[N - 1][N - 1];
 	}
 }
