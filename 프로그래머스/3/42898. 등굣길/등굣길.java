10c124608b372dc1c9c971fbcd11347e93ee9bf6
이진형 <86180422+Jinnyzinny@users.noreply.github.com>
2025-06-08 16:38:31 +0900

    [level 3] Title: 등굣길, Time: 0.80 ms, Memory: 53.7 MB -BaekjoonHub

--- 프로그래머스/3/42898. 등굣길/등굣길.java
+++ 프로그래머스/3/42898. 등굣길/등굣길.java
@@ -3,13 +3,19 @@ import java.awt.*;
 
 class Solution {
     private static int answer;
-    private static int n;
-    private static int m;
     private static boolean[][] hazards;
     
     private static int[][] memo;
-    
-    public void dfs(){
+
+    public int solution(int m, int n, int[][] puddles) {
+        answer = 0;
+        hazards=new boolean[n][m];
+        memo=new int[n][m];
+        
+        for(int[] puddle:puddles){
+            hazards[puddle[1]-1][puddle[0]-1]=true;
+        }
+        
         for(int row=0;row<n;row++){
             for(int col=0;col<m;col++){
                 if(row==0&&col==0){
@@ -22,20 +28,7 @@ class Solution {
                     memo[row][col]=(memo[row-1][col]+memo[row][col-1])%1000000007;
                 }
             }
-        }    
-    }
-    
-    public int solution(int m, int n, int[][] puddles) {
-        answer = 0;
-        this.n=n;
-        this.m=m;
-        hazards=new boolean[n][m];
-        memo=new int[n][m];
-        
-        for(int[] puddle:puddles){
-            hazards[puddle[1]-1][puddle[0]-1]=true;
-        }
-        dfs();
+        } 
         return memo[n-1][m-1]%1000000007;
     }
 }
