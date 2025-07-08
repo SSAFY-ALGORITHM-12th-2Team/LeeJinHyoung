c0b42b1e922738ff48a831308d5c1f9dd332689f
이진형 <86180422+Jinnyzinny@users.noreply.github.com>
2025-07-08 15:01:49 +0900

    [Gold IV] Title: 숨바꼭질 2, Time: 240 ms, Memory: 34188 KB -BaekjoonHub

---
+++ 백준/Gold/12851. 숨바꼭질 2/숨바꼭질 2.java
@@ -0,0 +1,67 @@
+import java.util.*;
+import java.util.stream.Stream;
+import java.io.*;
+
+public class Main {
+	static class move implements Comparable<move> {
+		int time;
+		int position;
+
+		public move(int time, int position) {
+			super();
+			this.time = time;
+			this.position = position;
+		}
+
+		@Override
+		public int compareTo(move o) {
+			// TODO Auto-generated method stub
+			return this.time - o.time;
+		}
+	}
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int[] input = Stream.of(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+
+		int N = input[0];
+		int K = input[1];
+
+		int[] position = new int[100001];
+		Arrays.fill(position, Integer.MAX_VALUE);
+		position[N] = 0;
+		// 경우의 수는 총 3개 +1 ,-1 *2
+//		결국은 순간이동으로 절댓갑스이 차가 가장 적은 곳으로 이동하는 게 핵심
+
+		int answerCount = 0;
+		Queue<move> q = new ArrayDeque<move>();
+		q.offer(new move(0, N));
+		while (!q.isEmpty()) {
+			move cur = q.poll();
+
+			if (cur.position < 0 || cur.position >= position.length)
+				continue;
+
+			if (cur.position == K) {
+				answerCount++;
+			}
+
+			if (cur.position + 1 < position.length && position[cur.position + 1] >= cur.time + 1) {
+				q.offer(new move(cur.time + 1, cur.position + 1));
+				position[cur.position + 1] = Math.min(position[cur.position] + 1, position[cur.position + 1]);
+			}
+			if (cur.position - 1 >= 0 && position[cur.position - 1] >= cur.time + 1) {
+				q.offer(new move(cur.time + 1, cur.position - 1));
+				position[cur.position - 1] = Math.min(position[cur.position] + 1, position[cur.position - 1]);
+			}
+			if (cur.position * 2 < position.length && position[cur.position * 2] >= cur.time + 1) {
+				q.offer(new move(cur.time + 1, cur.position * 2));
+				position[cur.position * 2] = Math.min(position[cur.position] + 1, position[cur.position * 2]);
+			}
+		}
+		System.out.println(position[K]);
+		System.out.println(answerCount);
+	}
+}
