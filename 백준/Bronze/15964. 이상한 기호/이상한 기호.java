900230077051f7302769174ea881185da45e5c99
이진형 <86180422+Jinnyzinny@users.noreply.github.com>
2025-07-11 12:08:19 +0900

    [Bronze V] Title: 이상한 기호, Time: 180 ms, Memory: 17776 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/15964. 이상한 기호/이상한 기호.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+
+		long A = sc.nextLong();
+		long B = sc.nextLong();
+
+		System.out.println((A + B) * (A - B));
+	}
+
+}
